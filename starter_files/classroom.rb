9f77fde4542852d226f8148aec323ca1dd4341fb
Maria Laura Tarabillo <mlta18@outlook.com>
2017-08-21 21:43:48 -0400

    finish the last three

--- starter_files/classroom.rb
+++ starter_files/classroom.rb
@@ -24,7 +24,8 @@ end
 # TIP: To convert an array like [[:indiana, 90], [:nevada, 80]] to a hash,
 # use .to_h. Also look at Hash#transform_values.
 def averages(grade_hash) # look at arrays
-
+  grade_hash.transform_values do |scores|
+    scores.sum / scores.length
 end
 
 # Return a letter grade for a numerical score.
@@ -34,17 +35,41 @@ end
 # 60-69 => D
 # < 60 => F
 def letter_grade(score)
+  case score
+  when 90..Float::INFINITY
+    "A"
+  when 80..89
+    "B"
+  when 70..79
+    "C"
+  when 60..69
+    "D"
+  when 0..59
+    "F"
+  end
+
 end
 
 # Return a hash of students and their final letter grade, as determined
 # by their average.
 def final_letter_grades(grade_hash)# combine average and letter_grade
+  grade.hash.transform_values do |scores|
+  average = scores.sum / scores.length
+  letter_grade(scores.sum / scores.length)
+  end
 end
 
 # Return the average for the entire class.
 def class_average(grade_hash)
+  hash = averages(grade_hash)
+  average_scores = hash.value
+  return average_scores.sum / average_scores.length
 end
 
 # Return an array of the top `number_of_students` students.
 def top_students(grade_hash, number_of_students)
+  hash_of_averages = averages(grade_hash)
+  sorted_hash_of_averages = hash_of_averages.sort_by { |student, average| -average }
+  top_students = sorted_hash_of_averages.first(number_of_students)
+  return top_students.to_h.keys.
 end
